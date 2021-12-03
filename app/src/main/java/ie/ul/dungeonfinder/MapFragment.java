435de7eed71149336be24f211b833f470bc9384b
rachel <58234819+rachycat@users.noreply.github.com>
2021-12-03 07:02:01 +0000

    Add files via upload

--- app/src/main/java/ie/ul/dungeonfinder/MapFragment.java
+++ app/src/main/java/ie/ul/dungeonfinder/MapFragment.java
@@ -9,11 +9,37 @@ import android.view.ViewGroup;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class MapFragment extends Fragment {
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-    @Nullable
+public class MapFragment extends Fragment implements OnMapReadyCallback {
+
+    GoogleMap map;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync( this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        map = googleMap;
+
+        LatLng Limerick = new LatLng( 52.663157, -8);
+        map.addMarker(new MarkerOptions().position(Limerick).title("Limerick"));
+        map.moveCamera(CameraUpdateFactory.newLatLng(Limerick));
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_map, container, false);
     }
 }
