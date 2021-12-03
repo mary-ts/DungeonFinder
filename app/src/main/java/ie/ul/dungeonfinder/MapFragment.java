1d9058d8fbf194acf5f12bdc1274b8ad00c0235f
rachel <58234819+rachycat@users.noreply.github.com>
2021-12-03 09:29:05 +0000

    Add files via upload

--- app/src/main/java/ie/ul/dungeonfinder/MapFragment.java
+++ app/src/main/java/ie/ul/dungeonfinder/MapFragment.java
@@ -1,11 +1,14 @@
 package ie.ul.dungeonfinder;
 
+import android.location.Address;
+import android.location.Geocoder;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
@@ -15,6 +18,10 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
 
 public class MapFragment extends Fragment implements OnMapReadyCallback {
 
@@ -33,9 +40,26 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
         map = googleMap;
 
-        LatLng Limerick = new LatLng( 52.663157, -8);
+        LatLng Limerick = new LatLng(52.663157, -8);
         map.addMarker(new MarkerOptions().position(Limerick).title("Limerick"));
         map.moveCamera(CameraUpdateFactory.newLatLng(Limerick));
+        map.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+            @Override
+            public void onMapClick(@NonNull LatLng latLng) {
+                try {
+                    Geocoder geocoder = new Geocoder(getContext(), Locale.ENGLISH);
+                    List<Address> addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
+                    MarkerOptions markerOptions = new MarkerOptions();
+                    markerOptions.position(latLng);
+                    markerOptions.title(addresses.get(0).getAddressLine(0));
+                    map.clear();
+                    map.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, map.getCameraPosition().zoom));
+                    map.addMarker(markerOptions);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 
     @Override
@@ -43,3 +67,5 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
         return inflater.inflate(R.layout.fragment_map, container, false);
     }
 }
+
+
