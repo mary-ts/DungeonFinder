1166db92f97ce50ac23bbf0bd2a4ff36d0eae5c3
Mary Tease <58113541+mary-ts@users.noreply.github.com>
2021-12-03 12:04:11 +0000

    Update MainActivity.java

--- app/src/main/java/ie/ul/dungeonfinder/MainActivity.java
+++ app/src/main/java/ie/ul/dungeonfinder/MainActivity.java
@@ -13,29 +13,29 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {    //this is what gets created when main activity is first told to run
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);   // this is bringing up the bottom navigation bar
         bottomNav.setOnNavigationItemSelectedListener(navListener);
 
     }
-    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =  //this is the event lisner for the bottom navigation bar
             new BottomNavigationView.OnNavigationItemSelectedListener() {
                 @Override
                 public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                     Fragment selectedFragment = null;
 
-                    switch (item.getItemId()) {
+                    switch (item.getItemId()) {  //switch case, this is how it knows which fragment to pull up when the icon on the botttom nav is tapped
                         case R.id.nav_map:
-                            selectedFragment = new MapFragment();
+                            selectedFragment = new MapFragment();   //pulls up a map 
                             break;
                         case R.id.nav_friends:
-                            selectedFragment = new FriendsFragment();
+                            selectedFragment = new FriendsFragment();  
                             break;
                         case R.id.nav_camp:
-                            selectedFragment = new CampaignFragment();
+                            selectedFragment = new CampaignFragment();  //pulls up the calendar
                             break;
                     }
                     getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
